79fcd1105c2892cd34d11f9da0f8811e09aa9d79
courtlandcampo <courtland.campo@ufl.edu>
2022-05-30 20:17:31 -0400

    Global variables established // pseudocode written

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+//GLOBAL VARIABLES
+
+var wordToGuess = document.getElementById(word-to-guess)
+var previousWord = document.getElementById(previous-word)
+var incorrectLetters = document.getElementById(incorrect-letters)
+var remainingGuesses = document.getElementById(remaining-guesses)
+
+var wins = document.getElementById(wins)
+var losses = document.getElementById(losses)
+
+document.onkeyup = function(e) {
+  //filter key presses
+  
+  console.log(e.key) //access user key press(es)
+
+  //pick random word from array
+
+  //compare picked word and letter + guesses
+
+  //wins and losses
+
+  //show previous guesses, correct and incorrect
+}
