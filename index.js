74a20cd83712f66b829c1179b850e1c9fb37365c
courtlandcampo <courtland.campo@ufl.edu>
2022-05-31 20:21:34 -0400

    Replaces letters with underscores

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var words = [
 
 //GLOBAL VARIABLES
 
-//var wordToGuess = document.getElementById(word-to-guess)
+var wordToGuess = document.querySelector('#word-to-guess')
 //var previousWord = document.getElementById(previous-word)
 //var incorrectLetters = document.getElementById(incorrect-letters)
 //var remainingGuesses = document.getElementById(remaining-guesses)
@@ -22,17 +22,23 @@ var words = [
 //var wins = document.getElementById(wins)
 //var losses = document.getElementById(losses)
 
+  //PICKING RANDOM WORD AND REPLACING W UNDERSCORE
+
+  var wordAtPlay = words[Math.floor(Math.random() * words.length)] //SELECTS RANDOM WORD
+
+  var answerArray = [] //PLACE TO HOLD UNDERSCORES
+  for (var i = 0; i < wordAtPlay.length; i++) { //REVIEWS LETTERS
+    answerArray[i] = "_"
+  }
+  wordToGuess.textContent = answerArray.join("")
+
 document.onkeyup = function() {
-  //filter key presses
+  //filter key presses?
 
   //console.log(key) //access user key press(es)
 
-  //pick random word from array
-  var wordAtPlay = words[Math.floor(Math.random() * words.length)]
-  console.log(wordAtPlay)
-
   //display number of underscores
-
+  
   //compare picked word and letter + guesses
 
   //wins and losses
